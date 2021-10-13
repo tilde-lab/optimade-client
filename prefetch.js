1872df7e46d6e78537f52b8fdb810c3a02646cf1
Valexr <valexr@gmail.com>
2021-10-13 22:01:53 +0300

    write to prefetched.json providers with limits

--- prefetch.js
+++ prefetch.js
@@ -11,9 +11,10 @@ const optimade = new Optimade({
 optimade.getProviders().then(async () => {
 
 	const providers = optimade.providers;
+	const apis = optimade.apis;
 
-	async function getMaxLimit(providers, filter = 'oqmd.org', max = 5000) {
-		const array = Object.entries(providers).filter(([k, v]) => !v.attributes.base_url.includes(filter));
+	async function getQueryLimits(providers, filter = 'oqmd.org', max = 5000) {
+		const filtered = Object.entries(providers).filter(([k, v]) => !v.attributes.base_url.includes(filter));
 
 		const fetchLimits = async (k, v) => {
 			const url = `${v.attributes.base_url}/v1/structures?filter=nelements=2&page_limit=${max}`;
@@ -27,14 +28,15 @@ optimade.getProviders().then(async () => {
 						: '0';
 				};
 				const nums = detail(res.errors).match(/\d+/g).filter(n => +n < max).map(n => +n);
-				// console.log(nums)
-				if (!nums.includes(0)) return { [k]: { ...v, attributes: { ...v.attributes, ['max_limit']: nums } } };
+				if (!nums.includes(0)) return {
+					[k]: { ...v, attributes: { ...v.attributes, ['query_limits']: nums } }
+				};
 			} catch (error) {
 				console.log(error);
 			}
 		};
 
-		providers = await array.reduce(async (promise, [k, v]) => {
+		providers = await filtered.reduce(async (promise, [k, v]) => {
 			const provider = await fetchLimits(k, v);
 			const acc = await promise;
 			return { ...acc, ...provider };
@@ -44,17 +46,12 @@ optimade.getProviders().then(async () => {
 		return providers;
 	}
 
-	fs.writeFile(path.join(__dirname, 'dist/providers.json'), JSON.stringify(providers), () => console.log('The providers file has been saved!'));
-	getMaxLimit(providers);
-
-	const data = {
-		providers: providers,
-		apis: optimade.apis,
-	};
-
-	fs.writeFile(path.join(__dirname, 'dist/prefetched.json'), JSON.stringify(data), (err) => {
-		if (err) throw err;
-		console.log('The cache file has been saved!');
+	getQueryLimits(providers).then(providers => {
+		const data = { providers, apis };
+		fs.writeFile(path.join(__dirname, 'dist/prefetched.json'), JSON.stringify(data), (err) => {
+			if (err) throw err;
+			console.log('The cache file has been saved!');
+		});
 	});
 
 });
